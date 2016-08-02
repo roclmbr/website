845c5ff8fb0019c5b048df763b540f5e649b4a01
roclmbr <roclmbr@gmail.com>
2016-08-02 15:12:37 -0300

    Made article limit bigger

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
     belongs_to :user
     
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
-    validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    validates :description, presence: true, length: {minimum: 10, maximum: 1000}
     has_many :article_categories
     has_many :categories, through: :article_categories
 end
